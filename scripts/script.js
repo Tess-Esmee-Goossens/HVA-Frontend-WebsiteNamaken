b35972116870b06bd59b67d111ba1eeb679c5502
Tess-Esmee-Goossens <tess.goossens@hva.nl>
2025-11-30 23:05:38 +0100

    Almost fully nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
 console.log("hi");
+
+/***************/
+/* toggle menu */
+/***************/
+var menu = document.querySelector("header > button");
+var nav = document.querySelector("header > nav");
+var body = document.querySelector("body");
+
+menu.onclick = toonMenu;
+
+function toonMenu() {
+    nav.classList.toggle("toonMenu");
+    body.classList.toggle("noscroll");
+}
